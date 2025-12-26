caab4d11ba8b8129aff38ede8b09d9604c28daf5
idlezombie <62874358+idlezombie@users.noreply.github.com>
2025-12-27 10:26:58 +1100

    correct code error

--- components/mpu6050_custom/sensor.py
+++ components/mpu6050_custom/sensor.py
@@ -13,10 +13,6 @@ from esphome.const import (
 mpu6050_ns = cg.esphome_ns.namespace("mpu6050_custom")
 MPU6050Custom = mpu6050_ns.class_("MPU6050Custom", cg.PollingComponent, i2c.I2CDevice)
 
-# -----------------------------
-# Sensor configuration keys
-# -----------------------------
-
 CONF_ACCEL_X = "accel_x"
 CONF_ACCEL_Y = "accel_y"
 CONF_ACCEL_Z = "accel_z"
@@ -30,16 +26,11 @@ CONF_TEMPERATURE = "temperature"
 CONF_ANGLE_X = "angle_x"
 CONF_ANGLE_Y = "angle_y"
 
-# -----------------------------
-# YAML Schema
-# -----------------------------
-
 CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(MPU6050Custom),
 
-            # Accelerometer sensors
             cv.Optional(CONF_ACCEL_X): sensor.sensor_schema(
                 unit_of_measurement=UNIT_G,
                 icon="mdi:axis-arrow",
@@ -56,7 +47,6 @@ CONFIG_SCHEMA = (
                 accuracy_decimals=3,
             ),
 
-            # Gyroscope sensors
             cv.Optional(CONF_GYRO_X): sensor.sensor_schema(
                 unit_of_measurement=UNIT_DEGREES_PER_SECOND,
                 icon="mdi:rotate-right",
@@ -73,14 +63,12 @@ CONFIG_SCHEMA = (
                 accuracy_decimals=3,
             ),
 
-            # Temperature
             cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
                 unit_of_measurement=UNIT_CELSIUS,
                 icon=ICON_THERMOMETER,
                 accuracy_decimals=1,
             ),
 
-            # Angles
             cv.Optional(CONF_ANGLE_X): sensor.sensor_schema(
                 unit_of_measurement=UNIT_DEGREES,
                 icon="mdi:angle-acute",
@@ -96,16 +84,11 @@ CONFIG_SCHEMA = (
     .extend(i2c.i2c_device_schema(0x68))
 )
 
-# -----------------------------
-# Code Generation
-# -----------------------------
-
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
-    # Accelerometer
     if CONF_ACCEL_X in config:
         sens = await sensor.new_sensor(config[CONF_ACCEL_X])
         cg.add(var.accel_x.set_parent(sens))
@@ -116,7 +99,6 @@ async def to_code(config):
         sens = await sensor.new_sensor(config[CONF_ACCEL_Z])
         cg.add(var.accel_z.set_parent(sens))
 
-    # Gyroscope
     if CONF_GYRO_X in config:
         sens = await sensor.new_sensor(config[CONF_GYRO_X])
         cg.add(var.gyro_x.set_parent(sens))
@@ -127,13 +109,13 @@ async def to_code(config):
         sens = await sensor.new_sensor(config[CONF_GYRO_Z])
         cg.add(var.gyro_z.set_parent(sens))
 
-    # Temperature
     if CONF_TEMPERATURE in config:
         sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
         cg.add(var.temperature.set_parent(sens))
 
-    # Angles
     if CONF_ANGLE_X in config:
         sens = await sensor.new_sensor(config[CONF_ANGLE_X])
         cg.add(var.angle_x.set_parent(sens))
-    if CONF
+    if CONF_ANGLE_Y in config:
+        sens = await sensor.new_sensor(config[CONF_ANGLE_Y])
+        cg.add(var.angle_y.set_parent(sens))
