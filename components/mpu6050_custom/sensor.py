d092434ade9b66128d21f59f6df84f1405ec4ef1
idlezombie <62874358+idlezombie@users.noreply.github.com>
2025-12-27 11:25:34 +1100

    add platform schema

--- components/mpu6050_custom/sensor.py
+++ components/mpu6050_custom/sensor.py
@@ -91,6 +91,8 @@ CONFIG_SCHEMA = (
     .extend(i2c.i2c_device_schema(0x68))
 )
 
+PLATFORM_SCHEMA = CONFIG_SCHEMA
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
